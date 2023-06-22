660398408402df30efbae6ace272ffaf9df8f7b0
B-atuti <brian.atuti@student.moringaschool.com>
2023-06-22 16:31:57 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+let employee = {
+    name: "Tamia Nova",
+    streetAddress: "412 DownSreet"
+};
+
+function updateEmployeeWithKeyAndValue( employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+
+function destuctivelyUpdateEmployeeWithAndValue(employee, key, value) [
+    employee[key] = value;
+    return employee;
+]
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
